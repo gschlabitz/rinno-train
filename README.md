4924548bc94e4133dc7f05db6f295ca69f216206
gschlabitz <guido.schlabitz@gmail.com>
2024-09-27 15:27:32 -0500

    Exercise 2: People Report by Fiscal Year

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Programming exercise for Application Developer position in the Office of the Vic
 
 This CLI tool reads training records from the specified input .json file and generates three output files:
 
-##Completed Totals
+##Exercise 1: Completed Totals
 The `completed_totals.json` output file lists all training programs in alphabetical order with a count of how many people have completed that training.
 
 ###Usage
@@ -14,4 +14,15 @@ python main.py --input trainings.json
 ```
 
 
+##Exercise 2: People That Completed Within Fiscal Year
+The `completion_report_by_year.json` output file lists all persons that have completed any of the specified training programs during the specified fiscal year.
 
+###Usage
+Specify the file path to the training program records JSON file.
+Specify the fiscal year.
+List training programs by name and in quotes as positional parameters.
+
+```
+python main.py -i trainings.json -y 2024 "Program 1" "Program 2"
+python main.py --input trainings.json --year 2024 "Program 1" "Program 2"
+```
--- main.py
+++ main.py
@@ -31,23 +31,64 @@ def count_program_completions(training_programs, training_records):
     return totals
 
 
+def is_within_fiscal_year(fiscal_year, timestamp):
+    try:
+        fiscal_year_start = datetime(fiscal_year-1, 7, 1)
+        fiscal_year_end = datetime(fiscal_year, 6, 30)
+        completion_date = datetime.strptime(timestamp, "%m/%d/%Y")
+        return fiscal_year_start <= completion_date <= fiscal_year_end
+    except:
+        return False
+
+
+def generate_completion_report_by_year(training_records, fiscal_year, training_programs):
+    report = {}
+    for program in training_programs:
+        report[program] = []
+        for record in training_records:
+            if "completions" in record:
+                for completion in record['completions']:
+                    if "name" in completion and completion['name'] == program:
+                        if is_within_fiscal_year(fiscal_year, completion['timestamp']):
+                            report[program].append(record['name'])
+
+        # sort and deduplicate aggregated people
+        report[program] = sorted(list(set(report[program])))
+
+    return report
+
+
 def main():
     # TODO: add paramter parsing
     # Using hardcoded parameters for now
     input_file = 'trainings.json'
-    training_records = None
+    training_program_queries = [
+        "Electrical Safety for Labs",
+        "X-Ray Safety",
+        "Laboratory Safety Training"
+    ]
+    fiscal_year = 2024
 
     # Load training data from specified JSON file
+    training_records = None
     with open(input_file, 'r') as file:
         training_records = json.load(file)
 
     training_programs = aggregate_training_programs(training_records)
+
+    # Excercise 1: Count how many people have completed each training
     completion_totals = count_program_completions(
         training_programs, training_records)
 
-    # Save completion totals as an ordered dictionary in JSON format
     with open("completion_totals.json", 'w') as file:
         json.dump(completion_totals, file, indent=4, default=str)
+
+    # Excercise 2: List everyone that completed a given training in a given fiscal year
+    completion_report_by_year = generate_completion_report_by_year(
+        training_records, fiscal_year, training_program_queries)
+
+    with open("completion_report_by_year.json", 'w') as file:
+        json.dump(completion_report_by_year, file, indent=4, default=str)
 
 
 if __name__ == '__main__':
