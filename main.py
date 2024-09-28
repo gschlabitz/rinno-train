a291a23e796a44cd01e9cbbdb17e817344afe31e
gschlabitz <guido.schlabitz@gmail.com>
2024-09-28 14:14:27 -0500

    catch input file errors, fix mixed quotes

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from datetime import datetime
 def aggregate_training_programs(completion_record):
     names = set()
     for record in completion_record:
-        if "completions" in record:
+        if 'completions' in record:
             for completion in record['completions']:
-                if "name" in completion:
+                if 'name' in completion:
                     names.add(completion['name'])
 
     return sorted(list(names))
@@ -16,7 +16,7 @@ def aggregate_training_programs(completion_record):
 
 def has_completed_training_program(program_name, completion_records):
     for completion in completion_records:
-        if "name" in completion and completion['name'] == program_name:
+        if 'name' in completion and completion['name'] == program_name:
             return True
     return False
 
@@ -26,7 +26,7 @@ def count_program_completions(training_programs, training_records):
     for program in training_programs:
         totals[program] = 0
         for record in training_records:
-            if "completions" in record:
+            if 'completions' in record:
                 if has_completed_training_program(program, record['completions']):
                     totals[program] += 1
     return totals
@@ -34,7 +34,7 @@ def count_program_completions(training_programs, training_records):
 
 def date_from_string(date_string):
     try:
-        return datetime.strptime(date_string, "%m/%d/%Y")
+        return datetime.strptime(date_string, '%m/%d/%Y')
     except:
         return None
 
@@ -47,14 +47,14 @@ def is_within_fiscal_year(fiscal_year, timestamp):
     return fiscal_year_start <= completion_date <= fiscal_year_end
 
 
-def generate_completion_report_by_year(training_records, fiscal_year, training_programs):
+def generate_completion_report_by_year(training_records, fiscal_year, program_filter):
     report = {}
-    for program in training_programs:
+    for program in program_filter:
         report[program] = []
         for record in training_records:
-            if "completions" in record:
+            if 'completions' in record:
                 for completion in record['completions']:
-                    if "name" in completion and completion['name'] == program:
+                    if 'name' in completion and completion['name'] == program:
                         if is_within_fiscal_year(fiscal_year, completion['timestamp']):
                             report[program].append(record['name'])
 
@@ -93,7 +93,7 @@ def generate_expiration_report_by_date(training_records, expiration):
     expiration_date = date_from_string(expiration)
     report = []
     for record in training_records:
-        if "completions" not in record:
+        if 'completions' not in record:
             continue
 
         entry = {'name': record['name']}
@@ -128,7 +128,7 @@ def generate_expiration_report_by_date(training_records, expiration):
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="""
+    parser = argparse.ArgumentParser(description='''
         Rinno Train is a reporting tool for training status of department
         employees. It generates 3 report files:
 
@@ -144,20 +144,21 @@ def parse_arguments():
         expiration_report_by_date.json:
         A list of all employees who have training programs that have expired
         or will expire within a month of the specified date.
-    """)
+    ''')
 
     parser.add_argument('-i', '--input_file', type=str, required=True,
                         help='Path to a training records JSON file.')
 
-    parser.add_argument('-x', '--expiration', type=str, required=True,
-                        help="""The expiration date for the expiration report 
+    parser.add_argument('-x', '--expiration', type=str, required=False,
+                        help='''The expiration date for the expiration report 
                         expressed in a quoted string in the format m/d/Y, 
-                        e.g. 2/29/2024.""")
+                        e.g. 2/29/2024. Defaults to today.''')
 
-    parser.add_argument('-y', '--fiscal_year', type=int, required=True,
-                        help='The fiscal year for the completion report.')
+    parser.add_argument('-y', '--fiscal_year', type=int, required=False,
+                        help='''The fiscal year for the completion report. 
+                        Defaults to the current year.''')
 
-    parser.add_argument('program_filter', nargs='+', help='''The names of the
+    parser.add_argument('program_filter', nargs='*', help='''The names of the
                         training programs to include in the completion report.
                         If no program names are specified, all programs will
                         be included.''')
@@ -167,34 +168,46 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
+    today = datetime.now()
 
     # Load training data from specified JSON file
     training_records = None
-    with open(args.input_file, 'r') as file:
-        training_records = json.load(file)
+    try:
+        with open(args.input_file, 'r') as file:
+            training_records = json.load(file)
+    except Exception as e:
+        print(e)
+        exit(1)
 
     # Report 1: Count how many people have completed each training
     training_programs = aggregate_training_programs(training_records)
     completion_totals = count_program_completions(
-        training_programs, training_records)
+        training_programs,
+        training_records
+    )
 
-    with open("completion_totals.json", 'w') as file:
+    with open('completion_totals.json', 'w') as file:
         json.dump(completion_totals, file, indent=4, default=str)
 
     # Report 2: List everyone that completed a given training
     # in a given fiscal year
     completion_report_by_year = generate_completion_report_by_year(
-        training_records, args.fiscal_year, args.program_filter or training_programs)
+        training_records,
+        fiscal_year=args.fiscal_year or today.year,
+        program_filter=args.program_filter or training_programs
+    )
 
-    with open("completion_by_year.json", 'w') as file:
+    with open('completion_by_year.json', 'w') as file:
         json.dump(completion_report_by_year, file, indent=4, default=str)
 
     # Report 3: List everyone whose training has expired or will expire
     # within a month of a given date.
     expiration_report_by_date = generate_expiration_report_by_date(
-        training_records, args.expiration)
+        training_records,
+        expiration=args.expiration or today.strftime('%m/%d/%Y')
+    )
 
-    with open("expiration_by_date.json", 'w') as file:
+    with open('expiration_by_date.json', 'w') as file:
         json.dump(expiration_report_by_date, file, indent=4, default=str)
 
 
